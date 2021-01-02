9079544e5be3d680619a20d9b9e8eb0c82a8c503
DePrincio <deprincio@DePrincio.local>
2021-01-01 22:47:14 -0500

    complete table data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Head from './Head';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = 'https://covid19.mathdro.id/api/deaths';
+  let initSelectedCountry = 'Canada';
+
+  const [myCountryData, setMyCountryData] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(initSelectedCountry);
+
+  useEffect(() => {
+    fetch(url)
+      .then(function (respo) {
+        return respo.json();
+      })
+      .then(function (data) {
+        data.forEach((x) => {
+          if (x.countryRegion === selectedCountry) {
+            setMyCountryData((prev) => [...prev, x]);
+          }
+        });
+      });
+  }, [selectedCountry]);
+
+  console.log(myCountryData);
+
+  function handleSelection(e) {
+    const country = e.target.value;
+    setSelectedCountry(country);
+    setMyCountryData([]);
+    // console.log(country);
+  }
+  if (myCountryData) {
+    return (
+      <div className='App'>
+        <Head />
+
+        <div className='table'>
+          <div className='countrySelection'>
+            <select onChange={handleSelection}>
+              <option value='Canada'>Canada</option>
+              <option value='Russia'>Russia</option>
+              <option value='US'>USA</option>
+            </select>
+          </div>
+          <table>
+            <thead>
+              <tr class='tblHeads'>
+                <th>Province</th>
+
+                <th>Infection</th>
+
+                <th>Recovered</th>
+
+                <th>Deaths</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {myCountryData.map((info, index) => {
+                return (
+                  <tr className='tblRows' key={index}>
+                    <td>{info.provinceState}</td>
+                    <td>{info.confirmed}</td>
+                    <td>{info.recovered}</td>
+                    <td>{info.deaths}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+  return <div></div>;
 }
 
 export default App;
